c94fdc2d8409c1eee5bd3f1fbd90ab79c6515e3b
quyen <naundylan@gmail.com>
2026-09-23 16:25:29 +0700

    fix: sửa phần code category

--- backend/src/app/modules/categories/router.py
+++ backend/src/app/modules/categories/router.py
@@ -2,14 +2,13 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.app.core.database import get_db
-from src.app.modules.products.model import Product
-from src.app.modules.products.schema import ProductResponse
+from src.app.modules.categories.model import Category
+from src.app.modules.categories.schema import CategoryResponse
 
-router = APIRouter(prefix="/products", tags=["Products"])
+router = APIRouter(prefix="/categories", tags=["Categories"])
 
 
-@router.get("/", response_model=list[ProductResponse])
-def get_products(db: Session = Depends(get_db)):
-    products = db.query(Product).all()
-
-    return products
+@router.get("/", response_model=list[CategoryResponse])
+def get_categories(db: Session = Depends(get_db)):
+    categories = db.query(Category).all()
+    return categories
--- backend/src/app/modules/categories/schema.py
+++ backend/src/app/modules/categories/schema.py
@@ -1,21 +1,10 @@
-from decimal import Decimal
-
 from pydantic import BaseModel, ConfigDict
 
 
-class ProductResponse(BaseModel):
-    product_id: int
-    product_name: str
-    price: Decimal
-    discount_percent: int | None = None
-    sale_price: Decimal | None = None
-    quantity: int
-    rating: Decimal | None = None
-    sold: int
-    brand: str | None = None
-    origin: str | None = None
-    image_src: str | None = None
-    description: str | None = None
-    category_id: int | None = None
+class CategoryResponse(BaseModel):
+    category_id: int
+    category_name: str
+    parent_id: int | None = None
+    slug: str
 
     model_config = ConfigDict(from_attributes=True)
