d22bfee3d68720e52015789bfbe57da41d1968eb
hoangngocnguyen <142553243+hoangngocnguyen@users.noreply.github.com>
2026-09-24 15:35:40 +0700

    fix: xóa phần khởi tạo FastAPI() lần 2 ở main.py

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -53,14 +53,6 @@ app.add_exception_handler(StarletteHTTPException, http_exception_handler)
 app.add_exception_handler(cloudinary.exceptions.Error, cloudinary_exception_handler)
 app.add_exception_handler(Exception, global_exception_handler)
 
-from src.app.modules.categories.router import router as category_router
-from src.app.modules.products.router import router as product_router
-
-app = FastAPI(title="Shop App API")
-
-app.include_router(product_router)
-app.include_router(category_router)
-
 
 @app.get("/")
 def root():
